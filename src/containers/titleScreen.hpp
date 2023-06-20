5870fe99750eb4769fb60bb31ba8bb7652fa86cf
Flávio <tminemcraft123@gmail.com>
2023-06-19 22:39:08 -0300

    Static TitleScreen

--- src/containers/titleScreen.hpp
+++ src/containers/titleScreen.hpp
@@ -5,37 +5,93 @@
 #include "../headers/engine.hpp"
 
 class TitleScreen {
-  // Start button
+  // Background
+
+  sf::Texture titleScreenBackgroundTexture;
+  sf::RectangleShape titleScreenBackgroundRect;
+  void initBackground() {
+    this->titleScreenBackgroundTexture.loadFromFile("src/assets/imgs/mountainBackground.png");
+    this->titleScreenBackgroundTexture.setSmooth(true);
+
+    this->titleScreenBackgroundRect.setSize(sf::Vector2f(1280, 720));
+    this->titleScreenBackgroundRect.setTexture(&this->titleScreenBackgroundTexture);
+  }
+
+  // Fontes
+
+  sf::Font harukazeFonte;
+  sf::Font yozakuraFonte;
+  void initFonte() {
+    this->harukazeFonte.loadFromFile("src/assets/fonts/Harukaze.otf");
+    this->yozakuraFonte.loadFromFile("src/assets/fonts/Yozakura.otf");
+  }
+
+  // Tittle
+
+  sf::Text titleText;
+  void initTitleText() {
+    this->titleText.setFont(this->harukazeFonte);
+    this->titleText.setString("Surround");
+    this->titleText.setCharacterSize(300);
+    this->titleText.setOrigin(this->titleText.getLocalBounds().width / 2, 0);
+    this->titleText.setFillColor(sf::Color(102, 0, 0));
+    this->titleText.setPosition(640, -50);
+  }
+
+  // Buttons
   Button startButton;
   void initStartButton() {
-    startButton.setSize(sf::Vector2f(100, 60));
-    startButton.setBackgroundColor(sf::Color(255, 255, 255));
-    startButton.setPosition(sf::Vector2f(0, 0));
+    sf::Text startButtonText;
+    startButtonText.setFont(yozakuraFonte);
+    startButtonText.setString("Start");
+    startButtonText.setCharacterSize(130);
+    startButtonText.setFillColor(sf::Color(36, 13, 13));
+
+    this->startButton.setPosition(sf::Vector2f(640, 330));
+    this->startButton.setText(startButtonText);
   }
 
   Button settingsButton;
   void initSettingsButton() {
-    settingsButton.setSize(sf::Vector2f(100, 60));
-    settingsButton.setBackgroundColor(sf::Color(255, 255, 255));
-    settingsButton.setPosition(sf::Vector2f(0, 70));
-    settingsButton.setOutlineSize(5);
-    settingsButton.setOutlineColor(sf::Color::Red);
+    sf::Text settingsButtonText;
+    settingsButtonText.setFont(yozakuraFonte);
+    settingsButtonText.setString("Settings");
+    settingsButtonText.setCharacterSize(130);
+    settingsButtonText.setFillColor(sf::Color(36, 13, 13));
+
+    this->settingsButton.setPosition(sf::Vector2f(640, 450));
+    this->settingsButton.setText(settingsButtonText);
   }
 
-  sf::Font fonte;
-  void initFonte() { fonte.loadFromFile("src/assets/fonts/JandaManateeSolid.ttf"); }
+  Button quitButton;
+  void initQuitButton() {
+    sf::Text quitButtonText;
+    quitButtonText.setFont(yozakuraFonte);
+    quitButtonText.setString("Quit");
+    quitButtonText.setCharacterSize(130);
+    quitButtonText.setFillColor(sf::Color(36, 13, 13));
+
+    this->quitButton.setPosition(sf::Vector2f(640, 580));
+    this->quitButton.setText(quitButtonText);
+  }
 
   void initVariables() {
-    initFonte();
-    initStartButton();
-    initSettingsButton();
+    this->initBackground();
+    this->initFonte();
+    this->initTitleText();
+    this->initStartButton();
+    this->initSettingsButton();
+    this->initQuitButton();
   }
 
  public:
   TitleScreen() { initVariables(); }
   void update() {}
   void render(sf::RenderWindow *pWindow) {
+    pWindow->draw(this->titleScreenBackgroundRect);
+    pWindow->draw(this->titleText);
     this->startButton.render(pWindow);
     this->settingsButton.render(pWindow);
+    this->quitButton.render(pWindow);
   }
 };
